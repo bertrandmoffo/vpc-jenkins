ee1d25f81d92caab9eecc394aafed368238d1f8e
bertrandmoffo <163623940+bertrandmoffo@users.noreply.github.com>
2024-06-09 00:14:00 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
  //creating a vpc
 
- resource "aws_vpc" "moffovpc" {
+ resource "aws_vpc" "kazi" {
    cidr_block = "44.32.0.0/16"
    instance_tenancy = "default"
    
    tags = {
-     Name = "moffovpc"
+     Name = "kazi"
    }
  }
